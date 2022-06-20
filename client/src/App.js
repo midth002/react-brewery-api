5c90ce83399aacd66586e01e5b3b8f69f85b161a
Andrew Midthun <midth002@gmail.com>
2022-06-20 13:14:44 -0500

    Initial commit with bulma navbar component.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NavbarTop from './components/navbar/NavbarTop';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarTop />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/NavbarTop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Navbar } from "react-bulma-components";
+// import Nav from "react-bootstrap/Nav";
+const NavbarTop = () => {
+  return (
+    <Navbar  aria-label='main navigation' color='dark'
+    brand='UI'>
+    </Navbar>
+  )
+}
+
+export default NavbarTop
