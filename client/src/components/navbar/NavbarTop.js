05b045facb9d620f05d4a21683e309c5f7faaafb
Andrew Midthun <midth002@gmail.com>
2022-06-20 13:42:18 -0500

    First instance of the navbar

--- client/src/components/navbar/NavbarTop.js
+++ client/src/components/navbar/NavbarTop.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Button, Navbar } from "react-bulma-components";
+import { Button, Navbar, Container, Columns, Column } from "react-bulma-components";
+import "./navbar.css";
 // import Nav from "react-bootstrap/Nav";
 const NavbarTop = () => {
   return (
-    <Navbar  aria-label='main navigation' color='dark'
+    <Navbar  aria-label='main navigation' className='nav'
     brand='UI'>
+    <Container className='link-container'>
+      <div class='columns'>
+        <div class="column">
+          <a href="" className='link'>HOME</a>
+        </div>
+        <div class="column">
+          <a href="" className='link'>LOCATION</a>
+        </div>
+        <div class="column">
+          <a href="" className='link'>SEARCH</a>
+        </div>
+        <div class="column">
+          <a href="" className='link'>LOGIN</a>
+        </div>
+      </div>
+    </Container>
     </Navbar>
   )
 }
