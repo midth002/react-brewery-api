a6577019cd6dcfab34a79ff5eb57cc56b3497fcb
Andrew Midthun <midth002@gmail.com>
2022-06-22 12:13:37 -0500

    hashrouter.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavbarTop from './components/navbar/NavbarTop';
 import WeatherBoard from './components/weatherBoard/WeatherBoard';
 import Location from './pages/location/Location';
 import Search from './pages/search/Search';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 function App() {
@@ -13,9 +13,9 @@ function App() {
     <NavbarTop />
     <Router>
       <Routes>
-      <Route path='/react-brewery-api' element={<Home />} />
-      <Route path='/react-brewery-api/search' element={<Search />} />
-        <Route path='/react-brewery-api/geolocation' element={<Location />} />
+      <Route path='/react-brewery-api/#/' element={<Home />} />
+      <Route path='/react-brewery-api/#/search' element={<Search />} />
+        <Route path='/react-brewery-api/#/geolocation' element={<Location />} />
       </Routes>
     </Router>
     {/* <Footer /> */}
